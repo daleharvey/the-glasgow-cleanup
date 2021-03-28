aac83de95e2275da9f17b576be2898cb423890d8
Dale Harvey <dale@arandomurl.com>
2021-03-28 18:09:51 +0100

    Added map navigation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let $ = document.querySelector.bind(document);
-let ctx = document.getElementById("map").getContext("2d");
+//let ctx = document.getElementById("map-putput").getContext("2d");
 
-function lon2tile(lon, zoom) { 
-  return (lon + 180) / 360 * Math.pow(2, zoom); 
+function lon2tile(lon, zoom) {
+  return (lon + 180) / 360 * Math.pow(2, zoom);
 }
 
-function lat2tile(lat, zoom) { 
+function lat2tile(lat, zoom) {
   return (
-    (1 - Math.log(Math.tan(lat * Math.PI / 180) + 
-    1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom)); 
+    (1 - Math.log(Math.tan(lat * Math.PI / 180) +
+    1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom));
 }
 
 let zoom = 18;
@@ -19,9 +19,9 @@ async function drawImage(ctx, url, x, y) {
   ctx.drawImage(img, x, y);
 }
 
-function parseGMaps(url) { 
+function parseGMaps(url) {
   let coords = url.replace("geo:", "").split("?")[0].split(",");
-  return { 
+  return {
     lat: parseFloat(coords[0]),
     lng: parseFloat(coords[1])
   }
@@ -37,60 +37,48 @@ const nth = function(d) {
   }
 }
 
-async function draw() {
-  let coords = parseGMaps($("#location").value);
-  console.log("Drawing coords", coords);
-  
-  let x = lat2tile(coords.lat, zoom);
-  let y = lon2tile(coords.lng, zoom);
-  
-  for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-      let mx = Math.floor(x - 1 + j);
-      let my = Math.floor(y - 1 + i);
-      
-      await drawImage(
-        ctx, 
-        `https://a.tile.openstreetmap.org/${zoom}/${my}/${mx}.png`, 
-        i * 256, 
-        j * 256
-      );
-    }
-  } 
-
-  let markerX = (Math.round((y - Math.floor(y)) * 256)) + 256 - 32;
-  let markerY = (Math.round((x - Math.floor(x)) * 256)) + 256 - 88;
-      
-  await drawImage(
-    ctx, 
-    "https://cdn.glitch.com/d12d647a-c769-4a20-b92a-8b40638b523b%2Fpin.svg?v=1616533602193", 
-    markerX, 
-    markerY, 64, 88
-  );
-  
-  let height = 80;
-  let d = new Date($("#date").value || Date.now());
-  let datestr = d.toLocaleString('en-us', {weekday: 'short'}) + " " + d.getDate() + 
-      nth(d.getDate()) + " of " + d.toLocaleString('en-us', {month:'short'}) 
-  let date = datestr + " @ " + $("#time").value;
-  let place = "Meet @ " + $("#name").value;
-
-  ctx.font = `${height}px sans-serif`;
-  ctx.fillStyle = "#000";
-  
-  let placeSize  = ctx.measureText(place);
-  let dateSize  = ctx.measureText(date);
-  
-  ctx.fillRect(1080 - (placeSize.width + 40 + 20), 1080 - ((2 * height) + 80), placeSize.width + 40, height + 20);
-  ctx.fillRect(1080 - (dateSize.width + 40 + 20), 1080 - (height + 40), dateSize.width + 40, height + 20);
-  
-  ctx.fillStyle = "#FFF";
-  ctx.fillText(place, 1080 - (placeSize.width + 40), 1080 - (height + 80));
-  ctx.fillText(date, 1080 - (dateSize.width + 40), 1080 - 40);
+function getLocationText() {
+  return  $("#name").value;
 }
 
-draw();
-$("#location").addEventListener("input", draw);
-$("#name").addEventListener("input", draw);
-$("#date").addEventListener("input", draw);
-$("#time").addEventListener("input", draw);
+function getTimeText() {
+  let d = new Date($("#date").value || Date.now());
+  let datestr = d.toLocaleString('en-us', {weekday: 'short'}) + " " + d.getDate() +
+      nth(d.getDate()) + " of " + d.toLocaleString('en-us', {month:'short'})
+  return datestr + " @ " + $("#time").value || "09:00";
+}
+
+function update() {
+  $("#location-output").innerText = getLocationText();
+  $("#time-output").innerText = getTimeText();
+}
+
+update();
+$("#name").addEventListener("input", update);
+$("#date").addEventListener("input", update);
+$("#time").addEventListener("input", update);
+
+$("#download").addEventListener("click", evt => {
+  evt.preventDefault();
+  $(".leaflet-control-zoom").style.visibility = "hidden";
+
+  domtoimage.toPng($("#map-wrapper")).then(dataUrl => {
+    var link = document.createElement('a');
+    link.download = 'glasgow-cleanup-map.png';
+    link.href = dataUrl;
+    link.click();
+    $(".leaflet-control-zoom").style.visibility = "visible";
+  }).catch(function (error) {
+    console.error('oops, something went wrong!', error);
+  });
+});
+
+let mymap = L.map('map').setView([55.8611, -4.2502], 18);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '',
+    maxZoom: 19,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZGFsZWhhcnZleSIsImEiOiJja21xdHFoeG0wMWV1MnZ0NGg5YTdha3llIn0.bx8FLtsoZC-J79G2PtYa_g'
+}).addTo(mymap);
